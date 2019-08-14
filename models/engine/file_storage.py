86852f23ab9f9773855acf7b5ce007465eacc6d8
Ferney Medina <fermed28@gmail.com>
2019-08-14 01:28:12 +0000

    [feature] add delete instance and select with filter option

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -20,12 +20,19 @@ class FileStorage:
     __file_path = "file.json"
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """returns a dictionary
         Return:
             returns a dictionary of __object
         """
-        return self.__objects
+        if cls is None:
+            return self.__objects
+        else:
+            aux = {}
+            for key, value in self.__objects.items():
+                if isinstance(value, cls):
+                    aux[key] = value
+            return aux
 
     def new(self, obj):
         """sets __object to given obj
@@ -55,3 +62,9 @@ class FileStorage:
                     self.__objects[key] = value
         except FileNotFoundError:
             pass
+
+    def delete(self, obj=None):
+        if obj and hasattr(obj, "id"):
+            key = "{}.{}".format(obj.__class__.__name__, obj.id)
+            del self.__objects[key]
+            self.save()
