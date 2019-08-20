f4dffb9196246aac5fa5604828f8041d27d9caff
GaviriaAmaya <judamayaga@unal.edu.co>
2019-08-20 03:40:05 +0000

    [Fix] Correcting if statement to install Nginx. Add option to the Symbolic link command

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -5,12 +5,11 @@ then
     sudo apt-get update
     sudo apt-get install nginx -y
 fi
-sudo apt-get -y update
-sudo apt-get -y install nginx
+
 sudo mkdir -p /data/web_static/releases/test/
 sudo mkdir -p /data/web_static/shared
 sudo echo "Hello, Test!" | sudo tee /data/web_static/releases/test/index.html
-sudo ln -sf /data/web_static/current /data/web_static/releases/test/
+sudo ln -snf /data/web_static/current /data/web_static/releases/test/
 sudo chown -R ubuntu:ubuntu /data/
 sed -i '72i\\tlocation /hbnb_static/ {\n\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-enabled/default
 service nginx restart
