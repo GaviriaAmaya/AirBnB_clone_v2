ea522001b70ebb599948d26eb13ece0e4bacfbd0
GaviriaAmaya <judamayaga@unal.edu.co>
2019-08-20 03:35:04 +0000

    [Feature] Add script with base configuration of Nginx in a server

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+#Setup Web Servers for deployment
+if [ ! -x /usr/sbin/nginx ];
+then
+    sudo apt-get update
+    sudo apt-get install nginx -y
+fi
+sudo apt-get -y update
+sudo apt-get -y install nginx
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared
+sudo echo "Hello, Test!" | sudo tee /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/current /data/web_static/releases/test/
+sudo chown -R ubuntu:ubuntu /data/
+sed -i '72i\\tlocation /hbnb_static/ {\n\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-enabled/default
+service nginx restart
