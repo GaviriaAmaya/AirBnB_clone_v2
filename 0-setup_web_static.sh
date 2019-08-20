dc55816d02e8a7885e8e6b35532db1267c72bcdb
GaviriaAmaya <judamayaga@unal.edu.co>
2019-08-20 04:42:57 +0000

    [Fix] Quit condition statement. Correcting Sed line and save into variable basic structure of HTML

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,14 +1,17 @@
 #!/usr/bin/env bash
 #Setup Web Servers for deployment
-if [ ! -x /usr/sbin/nginx ];
-then
-    sudo apt-get update
-    sudo apt-get install nginx -y
-fi
-
+sudo apt-get update
+sudo apt-get install nginx -y
 sudo mkdir -p /data/web_static/releases/test/
 sudo mkdir -p /data/web_static/shared
-sudo echo "Hello, Test!" | sudo tee /data/web_static/releases/test/index.html
+HelloTest="<html>
+<head>
+</head>
+<body>
+holberton school
+</body>
+</html>"
+sudo echo "$HelloTest" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/current /data/web_static/releases/test/
 sudo chown -R ubuntu:ubuntu /data/ && sudo chgrp -R ubuntu /data/
 sed -i '37i\\tlocation /hbnb_static/ {\n\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-enabled/default
