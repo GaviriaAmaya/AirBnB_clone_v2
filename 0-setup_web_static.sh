a9adbc825e5f3dfb85b76e7ed300413c4b9de24b
GaviriaAmaya <judamayaga@unal.edu.co>
2019-08-20 03:55:07 +0000

    [Fix] Restablishing sudo

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -2,14 +2,14 @@
 #Setup Web Servers for deployment
 if [ ! -x /usr/sbin/nginx ];
 then
-    apt-get update
-    apt-get install nginx -y
+    sudo apt-get update
+    sudo apt-get install nginx -y
 fi
 
-mkdir -p /data/web_static/releases/test/
-mkdir -p /data/web_static/shared
-echo "Hello, Test!" | sudo tee /data/web_static/releases/test/index.html
-ln -snf /data/web_static/current /data/web_static/releases/test/
-chown -R ubuntu:ubuntu /data/
-sed -i '72i\\tlocation /hbnb_static/ {\n\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-enabled/default
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared
+sudo echo "Hello, Test!" | sudo tee /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/current /data/web_static/releases/test/
+sudo chown -R ubuntu:ubuntu /data/ && sudo chgrp -R ubuntu /data/
+sed -i '37i\\tlocation /hbnb_static/ {\n\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-enabled/default
 service nginx restart
