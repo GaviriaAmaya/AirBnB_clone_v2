e97df922156e164083e4baaf783999bd94ae41a5
GaviriaAmaya <judamayaga@unal.edu.co>
2019-08-22 23:45:52 +0000

    [Fix] Modify sed

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -9,5 +9,5 @@ sudo mkdir -p /data/web_static/releases/test/ /data/web_static/shared/
 sudo echo "HelloTest!" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/current /data/web_static/releases/test/
 sudo chown -R ubuntu:ubuntu /data/
-sudo sed -i '72i\\tlocation /hbnb_static/ {\n\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-enabled/default
+sudo sed -i '72i\\tlocation /hbnb_static/ {\n\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-enabled/defaultx
 sudo service nginx restart
