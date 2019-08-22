2a60618a9159cc39fb6bf6188078da0604cf40bf
GaviriaAmaya <judamayaga@unal.edu.co>
2019-08-22 22:34:39 +0000

    [Fix] Correcting script

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,18 +1,13 @@
 #!/usr/bin/env bash
 #Setup Web Servers for deployment
-sudo apt-get update
-sudo apt-get install nginx -y
-sudo mkdir -p /data/web_static/releases/test/
-sudo mkdir -p /data/web_static/shared
-HelloTest="<html>
-<head>
-</head>
-<body>
-holberton school
-</body>
-</html>"
-sudo echo "$HelloTest" | sudo tee /data/web_static/releases/test/index.html
+if [ ! -x /usr/sbin/nginx ];
+then
+    sudo apt-get update
+    sudo apt-get install nginx
+fi
+sudo mkdir -p /data/web_static/releases/test/ /data/web_static/shared/
+sudo echo "HelloTest!" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/current /data/web_static/releases/test/
-sudo chown -R ubuntu:ubuntu /data/ && sudo chgrp -R ubuntu /data/
-sed -i '37i\\tlocation /hbnb_static/ {\n\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-enabled/default
-service nginx restart
+sudo chown -R ubuntu:ubuntu /data/
+sudo sed -i '72i\\tlocation /hbnb_static/ {\n\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-enabled/default
+sudo service nginx restart
