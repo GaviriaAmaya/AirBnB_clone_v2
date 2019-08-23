1a380658171622992cb6b8793c0c9249b2db313f
GaviriaAmaya <judamayaga@unal.edu.co>
2019-08-23 00:23:29 +0000

    [Fix] Index html done

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -7,7 +7,14 @@ then
 fi
 sudo mkdir -p /data/web_static/releases/test/
 sudo mkdir -p /data/web_static/shared
-sudo echo "HelloTest!" | sudo tee /data/web_static/releases/test/index.html
+Header='<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>'
+sudo echo "$Header" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/current /data/web_static/releases/test/
 sudo chown -R ubuntu:ubuntu /data/
 sudo sed -i '72i\\tlocation /hbnb_static/ {\n\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-enabled/default
