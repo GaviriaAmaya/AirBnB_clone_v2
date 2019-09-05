8d020ceef2d71c9878d0f4339139b29b4bd70ec5
GaviriaAmaya <judamayaga@unal.edu.co>
2019-09-05 04:25:08 +0000

    [Fix] Apply protection of execution and strict slashes False

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -2,7 +2,11 @@
 'Implementing a Minimal application for Flask'
 from flask import Flask
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 @app.route('/')
 def hello_hbnb():
     return 'Hello HBNB!'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
