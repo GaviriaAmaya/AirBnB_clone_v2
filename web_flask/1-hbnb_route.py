3321d901b25ed82c20a1cae43b8a82e24f2081a3
GaviriaAmaya <judamayaga@unal.edu.co>
2019-09-05 04:31:37 +0000

    [Feature] Define another decorator with display and runing in port 5000

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+'Implementing a Minimal application for Flask'
+from flask import Flask
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def hello_hbnb():
+    return 'Hello HBNB!'
+
+@app.route('/hbnb')
+def hbnb():
+    return 'HBNB'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0:5000')
