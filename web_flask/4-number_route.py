379bce514d3aa43fd1943f29fccf74b4631fa4ca
GaviriaAmaya <judamayaga@unal.edu.co>
2019-10-10 13:03:02 +0000

    [Feature] Add function that starts a web application, takes the query and determines if it's number

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+'Implementing a Minimal application for Flask'
+from flask import Flask
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def hello_hbnb():
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def hbnb():
+    return 'HBNB'
+
+
+@app.route('/c/<text>')
+def c(text):
+    return 'C ' + text.replace("_", " ")
+
+
+@app.route('/python/')
+@app.route('/python/<text>')
+def python(text='is_cool'):
+    return 'Python ' + text.replace("_", " ")
+
+@app.route('/number/<n>', strict_slashes=False)
+def number(n):
+    if (n.isdigit() is True):
+        return n + " is a number"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
